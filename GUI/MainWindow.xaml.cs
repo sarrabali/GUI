5cba6d39e338af5f105cc799bc83c722d7f243cd
sarah <sarah@MSI>
2023-11-11 14:33:50 -0500

    Add project files.

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
